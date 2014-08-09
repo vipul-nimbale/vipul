bf103803e985d5b2be4247d969e1945756c742c5
vipul-nimbale <vipul.nimbale@gmail.com>
2014-08-09 19:28:24 +0530

    GoEuroTest-final
    App to consume GoEuro Rest service

---
+++ GoEuroTest/src/com/geuro/constants/GeuroConstants.java
@@ -0,0 +1,51 @@
+package com.geuro.constants;
+
+public interface GeuroConstants {
+	
+	public static final String REQUEST_GET_METHOD = "GET";
+	
+	public static final String REQUEST_POST_METHOD = "POST";
+	
+	public static final String REQUEST_PROPERTY_1 = "Accept";
+	
+	public static final String REQUEST_PROPERTY_2 = "application/json";
+	
+	public static final String REST_URI = "http://api.goeuro.com/api/v2/position/suggest/en/";
+	
+	public static final String CITY_ID = "_id";
+	
+	public static final String CITY_TYPE = "type";
+	
+	public static final String CITY_COUNTRY="country";
+	
+	public static final String CITY_NAME = "name";
+	
+	public static final String CITY_GEO_POSITION = "geo_position";
+	
+	public static final String CITY_LATITUDE= "latitude";
+	
+	public static final String CITY_LONGITUDE= "longitude";
+	
+	public static final String CSV_FILE = "city.csv";
+	
+	public static final String COMMA_DELIMETER= ",";
+	
+	public static final String NEW_LINE = "\n";
+	
+	public static final String NO_ARGUMENT_ERROR_MESSAGE = "No argument passsed";
+	
+	public static final String CHAR_LIMIT_ERROR_MESSAGE = "city name should contain atleat two characters "+
+															" and No Special characters or numerical values";
+	
+	public static final String NO_RESPONSE_MESSAGE = "No Response from Service or invalid city name passed";
+	
+	public static final String FILE_ERROR_MESSAGE = "File does exist or it is being used by other process,close the file and run the program again";
+	
+	//Default values for pojo
+	
+	public static final String DEFAULT = "Not Available";
+	
+	
+			
+
+}
---
+++ GoEuroTest/src/src/com/geuro/bean/CityInfo.java
@@ -0,0 +1,52 @@
+package src.com.geuro.bean;
+
+public class CityInfo {
+	
+	private String _id;
+	private String country;
+	private String name;
+	private String type;
+	private String latitude;
+	private String longitude;
+	
+	
+	
+	public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+}
