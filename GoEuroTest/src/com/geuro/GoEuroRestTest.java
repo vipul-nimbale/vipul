682231e7162496c8a44ab564d3bf9bd06895ffa3
Vipul Nimbale <vipul.nimbale@gmail.com>
2014-08-13 16:01:43 +0530

    Update GoEuroRestTest.java

--- GoEuroTest/src/com/geuro/GoEuroRestTest.java
+++ GoEuroTest/src/com/geuro/GoEuroRestTest.java
@@ -20,7 +20,7 @@ import org.json.simple.parser.ParseException;
 import com.geuro.bean.CityInfo;
 import static com.geuro.constants.GeuroConstants.*;
 
-public class GoEuroRestTest implements GeuroConstants  {
+public class GoEuroRestTest   {
 
 	/**
 	 * @param args
